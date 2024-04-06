1453d25e56300fe9423e15114018ef6ac8d2ee8a
nishntraut17 <nishntraut17@gmail.com>
2024-04-07 00:21:40 +0530

    changed whatsapp office number

--- src/components/extra/Whatsapp.tsx
+++ src/components/extra/Whatsapp.tsx
@@ -5,7 +5,7 @@ export default function Whatsapp() {
 
     return (
         <div className='fixed bottom-14 left-5 z-[1200]'>
-            <Link to="https://wa.me/+919699375115" target="_blank" className="flex flex-row">
+            <Link to="https://wa.me/+918275017003" target="_blank" className="flex flex-row">
                 <img src="/whatsapp.png" alt="whatsapp" width={75} height={75} />
             </Link>
         </div>
