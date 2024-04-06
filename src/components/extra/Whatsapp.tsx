a8ddf5a469ee46c4a3d7ceb143367fcdf0458f1a
nishntraut17 <nishntraut17@gmail.com>
2024-04-06 17:08:13 +0530

    further correction of products done

--- src/components/extra/Whatsapp.tsx
+++ src/components/extra/Whatsapp.tsx
@@ -19,7 +19,7 @@ export default function Whatsapp() {
 
     return (
         <div className='fixed bottom-14 left-5 z-[1200]'>
-            <Link to="https://wa.me/9699375115" target="_blank" className="flex flex-row">
+            <Link to="https://wa.me/+919699375115" target="_blank" className="flex flex-row">
                 <img src="/whatsapp.png" alt="whatsapp" width={75} height={75} />
                 {/* {showHoverText && <span className="bg-white h-8 z-[2000px] rounded-lg">Chat on WhatsApp</span>} */}
             </Link>
